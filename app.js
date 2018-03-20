45ea1b3201d2a170cb0c1240f7554cb2fca1f7df
Andres <andresrod21@gmail.com>
2018-03-20 10:50:09 -0400

    Got partials and links working

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const path = require('path');
 var app = express();
 
 //VIEW ENGINE SETUP
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts', partialsDir: __dirname + 'views/partials/'}));
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'layout',
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+}));
 // app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -42,21 +47,23 @@ app.get('/about', (req, res) => {
 app.get('/business-consulting', (req, res) => {
   res.render('business-consulting.hbs', {
     pageTitle: 'Business Consulting',
-  });
-});
-
-app.get('/product-development', (req, res) => {
-  res.render('product-development.hbs', {
-    pageTitle: 'Product Development',
+    active1: 'active'
   });
 });
 
 app.get('/brand-identity', (req, res) => {
   res.render('brand-identity.hbs', {
     pageTitle: 'Brand Identity',
+    active2: 'active'
   });
 });
 
+app.get('/product-development', (req, res) => {
+  res.render('product-development.hbs', {
+    pageTitle: 'Product Development',
+    active3: 'active'
+  });
+});
 
 app.listen(port, () => {
   console.log('Server is up on port', port);
